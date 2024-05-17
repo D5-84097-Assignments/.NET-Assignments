11c003f149be17495969f8200e990734b29a8fbd
Amit-Raskar <amitraskar09@gmail.com>
2024-05-17 17:30:24 +0530

    Q1 uploaded

---
+++ Assignments/Q-01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Q_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
