1becccc4ad80381a6576fb78c0474f3d05943dd5
Amit-Raskar <amitraskar09@gmail.com>
2024-05-17 17:32:39 +0530

    Q02 uploaded

---
+++ Assignments/Q-02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Q_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(args[0]);
+            int num2 = Convert.ToInt32(args[2]);
+            switch (args[1]) 
+            {
+                case "+":
+                    Console.WriteLine("Addition is = "+ (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine("Subtraction is = " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine("Multiplication is = " + (num1 * num2));
+                    break;
+                case "/":
+                    Console.WriteLine("Division is = " + (num1 / num2));
+                    break;
+            }
+        }
+    }
+}
