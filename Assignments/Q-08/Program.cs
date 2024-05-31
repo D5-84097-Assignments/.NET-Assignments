b00d89c6d60d9e728bb735befaeb50513a24b958
Amit-Raskar <amitraskar09@gmail.com>
2024-05-31 17:54:45 +0530

    Q-08 uploaded

---
+++ Assignments/Q-08/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Supervisor supervisor = new Supervisor();
+            supervisor.Accept();
+            supervisor.Print(); 
+            Console.ReadLine();
+        }
+    }
+}
