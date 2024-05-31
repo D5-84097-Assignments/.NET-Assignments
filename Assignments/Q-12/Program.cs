1315fadfe34e27fe4186c2beb75dc3e345ec7abf
Amit-Raskar <amitraskar09@gmail.com>
2024-05-31 17:58:41 +0530

    Q-12 uploaded

---
+++ Assignments/Q-12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company = new Company();
+            company.Accept();
+            Console.WriteLine("Enter number of employees");
+            int empCount = Convert.ToInt32(Console.ReadLine()); 
+            for(int i = 0; i < empCount; i++)
+            {
+                company.AddEmployee();
+            }
+            company.Print();
+            company.DisplayEmployees();
+            Console.ReadLine();
+        }
+    }
+}
