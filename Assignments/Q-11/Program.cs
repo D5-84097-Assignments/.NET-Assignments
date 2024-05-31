b70168109ba15d875ec2fab26457ac931324663c
Amit-Raskar <amitraskar09@gmail.com>
2024-05-31 17:57:58 +0530

    Q-11 uploaded

---
+++ Assignments/Q-11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WageEmp wageEmp = new WageEmp();
+            wageEmp.Accept();
+            wageEmp.Print();
+            Console.ReadLine();
+        }
+    }
+}
